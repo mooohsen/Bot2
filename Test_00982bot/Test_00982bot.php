d9a780a1fd33ea4bcd25e436260ea6f7c471cde0
mooohsen <mohamad.mohsen1166@gmail.com>
2020-07-31 20:04:17 +0430

    Update Test_00982bot.php

--- Test_00982bot/Test_00982bot.php
+++ Test_00982bot/Test_00982bot.php
@@ -6,11 +6,31 @@ $telegram = new Telegram($bot_id);
 $text 			 	 = $telegram->Text();	
 $chat_id 		 	 = $telegram->ChatID();
 $msgType		  	 = $telegram->getUpdateType();
+if($text=='fr')
+{
+	$myfile = fopen("a.txt", "r") or die("Unable to open file!");
+	echo $m = fread($myfile,$n=filesize("a.txt"));
+	fclose($myfile);
 
-$myfile = fopen("a.txt", "r") or die("Unable to open file!");
-echo $m = fread($myfile,$n=filesize("a.txt"));
-fclose($myfile);
+	//$content = ['chat_id' => $chat_id, 'text' => $msgType.' '.$text];
+	$content = ['chat_id' => $chat_id, 'text' => $m.PHP_EOL.$n];
+	$telegram->sendMessage($content);
+}
 
-//$content = ['chat_id' => $chat_id, 'text' => $msgType.' '.$text];
-$content = ['chat_id' => $chat_id, 'text' => $m.' '.$n];
-$telegram->sendMessage($content);
+
+
+if($text=='fw')
+{
+	
+	$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+	$txt = "John Doe\n";
+	fwrite($myfile, $txt);
+	$txt = "Jane Doe\n";
+	fwrite($myfile, $txt);
+	fclose($myfile);
+
+	//$content = ['chat_id' => $chat_id, 'text' => $msgType.' '.$text];
+	$content = ['chat_id' => $chat_id, 'text' => 'File White'];
+	$telegram->sendMessage($content);
+}
+?>
